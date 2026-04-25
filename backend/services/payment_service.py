04e4b293c91718ca850ca6e3850f3252c1e2ef92
Avinash Kumar <amit.yadaw@gmail.com>
2026-04-25 21:16:26 +0530

    changes

--- backend/services/payment_service.py
+++ backend/services/payment_service.py
@@ -1,13 +1,23 @@
 import os
-import razorpay
+try:
+    import razorpay
+except ImportError:
+    razorpay = None
 from models import Donation
 from services.receipt_service import generate_receipt
 
-razorpay_client = razorpay.Client(
-    auth=(os.getenv("RAZORPAY_KEY_ID"), os.getenv("RAZORPAY_KEY_SECRET"))
+if razorpay:
+    razorpay_client = razorpay.Client(
+        auth=(os.getenv("RAZORPAY_KEY_ID"), os.getenv("RAZORPAY_KEY_SECRET"))
 )
 
 def verify_payment_and_save(data, db):
+    if not razorpay:
+        raise ValueError("Razorpay is not installed")
+
+    if not razorpay_client:
+        raise ValueError("Razorpay client is not initialized")
+
     razorpay_client.utility.verify_payment_signature({
         'razorpay_order_id': data['razorpay_order_id'],
         'razorpay_payment_id': data['razorpay_payment_id'],
